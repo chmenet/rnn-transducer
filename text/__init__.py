b0ffed718a7739bf023952eff00f4a34e513e3a1
YeongTae <hwak1234@gmail.com>
2019-09-05 11:50:05 +0900

    debugging korean embedding

--- text/__init__.py
+++ text/__init__.py
@@ -3,15 +3,10 @@ import re
 from text import cleaners
 from text.symbols import eng_symbols, kor_symbols
 from text.unicode import join_jamos
-
 from jamo import j2hcj
-from sys import stderr
-
-import os
-import json
+from hangul_jamo import compose
 
 cleaner_names = ["korean_cleaners"]
-_ROOT = os.path.abspath(os.path.dirname(__file__))
 
 # Mappings from symbol to numeric ID and vice versa:
 symbols = ""
@@ -20,45 +15,6 @@ _id_to_symbol = {}
 
 # Regular expression matching text enclosed in curly braces:
 _curly_re = re.compile(r'(.*?)\{(.+?)\}(.*)')
-
-with open(os.path.join(_ROOT, "decompositions.json"), 'r') as namedata:
-  _JAMO_TO_COMPONENTS = json.load(namedata)
-_COMPONENTS_REVERSE_LOOKUP = {tuple(comps): char for char, comps in _JAMO_TO_COMPONENTS.items()}
-
-
-class InvalidJamoError(Exception):
-  """jamo is a U+11xx codepoint."""
-
-  def __init__(self, message, jamo):
-    super(InvalidJamoError, self).__init__(message)
-    self.jamo = hex(ord(jamo))
-    print("Could not parse jamo: U+{code}".format(code=self.jamo[2:]),
-          file=stderr)
-
-def compose_jamo(*parts):
-  """Return the compound jamo for the given jamo input.
-  Integers corresponding to U+11xx jamo codepoints, U+11xx jamo
-  characters, or HCJ are valid inputs.
-
-  Outputs a one-character jamo string.
-  """
-  # Internally, we convert everything to a jamo char,
-  # then pass it to _jamo_to_hangul_char
-  # NOTE: Relies on hcj_to_jamo not strictly requiring "position" arg.
-  for p in parts:
-    if not (type(p) == str and len(p) == 1 and 2 <= len(parts) <= 3):
-      raise TypeError("compose_jamo() expected 2-3 single characters " +
-                      "but received " + str(parts),
-                      '\x00')
-  hcparts = [j2hcj(_) for _ in parts]
-  hcparts = tuple(hcparts)
-  if hcparts in _COMPONENTS_REVERSE_LOOKUP:
-    return _COMPONENTS_REVERSE_LOOKUP[hcparts]
-  raise InvalidJamoError(
-    "Could not synthesize characters to compound: " + ", ".join(
-      str(_) + "(U+" + str(hex(ord(_)))[2:] +
-      ")" for _ in parts), '\x00')
-
 
 def change_symbol(cleaner_names):
   symbols = ""
@@ -100,14 +56,13 @@ def text_to_sequence(text, cleaner_names):
     except:
       print(text)
       exit()
-  # Append EOS token
-  #if cleaner_names == ["korean_cleaners"]: sequence.append(_symbol_to_id['~'])
   return sequence
 
 
-def sequence_to_text(sequence):
+def sequence_to_text(sequence, cleaner_names=["korean_cleaners"]):
   '''Converts a sequence of IDs back to a string'''
   result = ''
+
   for symbol_id in sequence:
     if symbol_id in _id_to_symbol:
       s = _id_to_symbol[symbol_id]
@@ -115,7 +70,12 @@ def sequence_to_text(sequence):
       if len(s) > 1 and s[0] == '@':
         s = '{%s}' % s[1:]
       result += s
-  return result.replace('}{', ' ')
+  results = result.replace('}{', ' ')
+
+  if(cleaner_names == ["korean_cleaners"]):
+    results = compose(j2hcj(result))
+
+  return results
 
 
 def _clean_text(text, cleaner_names):
@@ -140,6 +100,7 @@ def _should_keep_symbol(s):
 if __name__ == "__main__":
   print(text_to_sequence('this is test sentence.? ', ['english_cleaners']))
   print(text_to_sequence('테스트 문장입니다.? ', ['korean_cleaners']))
+  print(sequence_to_text(text_to_sequence('테스트 문장입니다.? ', ['korean_cleaners'])))
   print(_clean_text('AB테스트 문장입니다.? ', ['korean_cleaners']))
   print(_clean_text('mp3 파일을 홈페이지에서 다운로드 받으시기 바랍니다.',['korean_cleaners']))
   print(_clean_text("마가렛 대처의 별명은 '철의 여인'이었다.", ['korean_cleaners']))
