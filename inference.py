a1dd535f6cd8f1eaece91849cb5481086a4818fa
chme <chme@netmarble.com>
2019-09-10 06:50:36 +0000

    debugging inference.py

--- inference.py
+++ inference.py
@@ -26,7 +26,8 @@ def audio_path_to_text(audio_path, model, stft, config):
     if config.training.num_gpu > 0:
         mel, mel_lengths = mel.cuda(), mel_lengths.cuda()
     recog_indexes = model.recognize(mel, mel_lengths)
-    result_seq = [r[0] for r in recog_indexes[0]]
+    #print(recog_indexes)
+    result_seq = [r for r in recog_indexes[0]]
     return sequence_to_text(result_seq)
 
 def inference(config, cpath, ipath):
@@ -57,6 +58,7 @@ if __name__ == '__main__':
     """
         usage
         python inference.py -config egs/AIhub/exp/aihub_test/config.yaml -cpath egs/AIhub/exp/aihub_test/aihub_test.epoch0.chkpt -ipath aihub_test.txt
+        python inference.py -config egs/AIhub/exp/AIhub/config.yaml -cpath ./egs/AIhub/exp/aihub_test_beam_0910.chkpt -ipath aihub_test.txt
     """
     parser = argparse.ArgumentParser()
     parser.add_argument('-config', type=str, default='config/aihub.yaml')
