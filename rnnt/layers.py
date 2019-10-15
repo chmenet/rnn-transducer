41ad4fac9c93aac1d804b46dfc71eca022c922a0
YeongTae <hwak1234@gmail.com>
2019-10-15 15:39:14 +0900

    normalized feature
    Signed-off-by: YeongTae <hwak1234@gmail.com>

--- rnnt/layers.py
+++ rnnt/layers.py
@@ -1,9 +1,8 @@
 import torch
 import numpy as np
 from librosa.filters import mel as librosa_mel_fn
-from rnnt.audio_processing import dynamic_range_compression
-from rnnt.audio_processing import dynamic_range_decompression
-from rnnt.stft import STFT
+from audio_processing import dynamic_range_compression, dynamic_range_decompression, mel_normalize, mel_denormalize
+from stft import STFT
 
 def dct(x, norm=None):
     """
@@ -76,6 +75,7 @@ class TacotronSTFT(torch.nn.Module):
         self.n_mel_channels = hparams.n_mel_channels
         self.sampling_rate = hparams.sampling_rate
         self.stft_fn = STFT(hparams.filter_length, hparams.hop_length, hparams.win_length)
+        self.max_abs_mel_value = hparams.max_abs_mel_value
         mel_basis = librosa_mel_fn(
             hparams.sampling_rate, hparams.filter_length, hparams.n_mel_channels, hparams.mel_fmin, hparams.mel_fmax)
         mel_basis = torch.from_numpy(mel_basis).float()
@@ -89,12 +89,48 @@ class TacotronSTFT(torch.nn.Module):
         output = dynamic_range_decompression(magnitudes)
         return output
 
-    def cepstrum_from_mel(self, mel):
-        #magnitudes = self.spectral_de_normalize(mel)
-        mcc = dct(mel,'ortho')
+    def transform2(self, y):
+        result = torch.stft(y, n_fft=1024, hop_length=256, win_length =1024)
+        print(result.shape)
+        real = result[:, :, :, 0]
+        imag = result[:, :, :, 1]
+        magnitude = torch.sqrt(real ** 2 + imag ** 2)
+        phase = torch.autograd.Variable(
+            torch.atan2(imag.data, real.data))
+        print(phase.shape, phase.min(), phase.max())
+        print(magnitude.shape, magnitude.min(), magnitude.max())
+        return magnitude, phase
+
+    def cepstrum_from_mel(self, mel, ref_level_db = 20, magnitude_power=1.5):
+        assert (torch.min(mel.data) >= -self.max_abs_mel_value)
+        assert (torch.max(mel.data) <= self.max_abs_mel_value)
+        #print('mel: ', mel.max(), mel.min())
+        spec = mel_denormalize(mel, self.max_abs_mel_value)
+        #print('spec: ', spec.max(), spec.min())
+        magnitudes = self.spectral_de_normalize(spec + ref_level_db) ** (1 / magnitude_power)
+        #print('Magnitude: ', Magnitude.max(), Magnitude.min())
+        pow_spec = (magnitudes**2)/1024 # if filter_length = 1024
+        #print('pow_spec: ', pow_spec.max(), pow_spec.min())
+        db_pow_spec = torch.log(torch.clamp(pow_spec,min=1e-5))*20 #db
+        #print('db_pow_spec: ', db_pow_spec.max(), db_pow_spec.min())
+        mcc = dct(db_pow_spec,'ortho' )
         return mcc
 
-    def mel_spectrogram(self, y):
+    def cepstrum_from_audio(self, y):
+        assert (torch.min(y.data) >= -1)
+        assert (torch.max(y.data) <= 1)
+        magnitudes, phases = self.stft_fn.transform(y)
+        #print('magnitudes: ', magnitudes.max(), magnitudes.min())
+        pow_spec = (1/1024)*magnitudes**2
+        #print('pow_spec: ', pow_spec.max(), pow_spec.min())
+        mel_spectrogram = torch.matmul(self.mel_basis, pow_spec).squeeze(0).transpose(0,1)
+        #print('mel_spectrogram: ', mel_spectrogram.max(), mel_spectrogram.min())
+        db_mel_spectrogram = torch.log10(torch.clamp(pow_spec,min=1e-5))*20 #db
+        #print('db_mel_spectrogram: ', db_mel_spectrogram.max(), db_mel_spectrogram.min())
+        mcc = dct(db_mel_spectrogram,'ortho')
+        return mcc
+
+    def mel_spectrogram(self, y, ref_level_db = 20, magnitude_power=1.5):
         """Computes mel-spectrograms from a batch of waves
         PARAMS
         ------
@@ -107,8 +143,18 @@ class TacotronSTFT(torch.nn.Module):
         assert(torch.min(y.data) >= -1)
         assert(torch.max(y.data) <= 1)
 
+        #print('y' ,y.max(), y.mean(), y.min())
         magnitudes, phases = self.stft_fn.transform(y)
         magnitudes = magnitudes.data
-        mel_output = torch.matmul(self.mel_basis, magnitudes)
-        mel_output = self.spectral_normalize(mel_output)
+        #print('stft_fn', magnitudes.max(), magnitudes.mean(), magnitudes.min())
+        mel_output = torch.matmul(self.mel_basis, torch.abs(magnitudes)**magnitude_power)
+        #print('_linear_to_mel', mel_output.max(), mel_output.mean(), mel_output.min())
+        mel_output = self.spectral_normalize(mel_output) - ref_level_db
+        #print('_amp_to_db', mel_output.max(), mel_output.mean(), mel_output.min())
+        mel_output = mel_normalize(mel_output, self.max_abs_mel_value)
+        #print('_normalize', mel_output.max(), mel_output.mean(), mel_output.min())
+        #spec = mel_denormalize(mel_output, self.max_abs_mel_value)
+        #print('_denormalize', spec.max(), spec.mean(), spec.min())
+        #spec = self.spectral_de_normalize(spec + ref_level_db)**(1/magnitude_power)
+        #print('db_to_amp', spec.max(), spec.mean(), spec.min())
         return mel_output
